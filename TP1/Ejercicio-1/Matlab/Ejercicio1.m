3cebf2e8574570b45a8077a2aedd316faf016a84
tlondero <tlondero@itba.edu.ar>
2020-03-31 17:56:21 -0300

    Update Ejercicio1.m

--- TP1/Ejercicio-1/Matlab/Ejercicio1.m
+++ TP1/Ejercicio-1/Matlab/Ejercicio1.m
@@ -1,19 +1,17 @@
-function [med, sigma] = ejercicio1(lambda, samples, bins)
+function [med, sigma2] = ejercicio1(lambda, samples, bins)
     unif = unifrnd(0,1,[1 samples]);
     exp_dist = log(1./(1 - unif))/lambda;
     
     aux = 0:4/samples:4 - 4/samples;
     exp_teo = lambda*exp(-lambda*aux);
     
-    h = histogram(exp_dist,'Normalization','probability');
+    h = histogram(exp_dist, bins,'Normalization','probability');
     [ymax, xmax] = max(h.Values);
-    %h = hist(exp_dist, bins);
-    %h = histfit(exp_dist, bins, 'exponential');
-    
+       
     hold on
     p = plot(aux, exp_teo*ymax/exp_teo(1));
     p(1).LineWidth = 2.5;
     
     med = mean(exp_dist);
-    sigma = std(exp_dist)^2;
+    sigma2 = std(exp_dist)^2;
 end
