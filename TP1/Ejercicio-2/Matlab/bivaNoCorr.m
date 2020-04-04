e0b753c1dc6c0a7501f994d48917e3ca67cb5846
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2020-04-04 17:56:18 -0300

    CorrBiva

--- TP1/Ejercicio-2/Matlab/bivaNoCorr.m
+++ TP1/Ejercicio-2/Matlab/bivaNoCorr.m
@@ -1,11 +1,12 @@
-function [ x,y ] = bivaNoCorr( mean,sigma, muestras )
+function [ x,y ] = bivaNoCorr( mean1, mean2, sigma1, sigma2, muestras )
 %BIVANOCORR 2 vectores de muestras con valores dist normal no correlacionados
 %   Uso las funciones rayleigh_dist y theta_dist para encontrar valores
 %   para radio y angulo de las muestras. Dev
-ray = rayleigh_dist(sigma,muestras);
+ray1 = rayleigh_dist(sigma1,muestras);
+ray2 = rayleigh_dist(sigma2, muestras);
 thet = theta_dist(muestras);
-x = ray.*(cos(thet));
-y = ray.*sin(thet);
+x = ray1.*(cos(thet)) + mean1;
+y = ray2.*sin(thet) + mean2;
 
 end
 
