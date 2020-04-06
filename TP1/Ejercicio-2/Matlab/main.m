fb26feb84b1885c3c81ab89bfbfad42d2e0c0e8e
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2020-04-06 15:26:24 -0300

    Funciona :100:  :sweat_drops:  :sweat_drops:  :eggplant:

--- TP1/Ejercicio-2/Matlab/main.m
+++ TP1/Ejercicio-2/Matlab/main.m
@@ -1,4 +1,4 @@
-function [X, Y2, meanX, meanY, A, Ainv, sigX, sigY] = main(mean1, mean2, sigx1, sigx2, corr, samples)
+function [X, Y2, meanX, meanY, A, A0, sigX, sigY] = main(mean1, mean2, sigx1, sigx2, corr, samples)
 
 % 1) Creamos las V.A. correspondientes al vector Y.
 [y1, y2] = genX(sigx1, sigx2, samples);
@@ -9,7 +9,11 @@ meanX = [mean1; mean2];
 sigX = [sigx1*sigx1 corr*sigx1*sigx2; corr*sigx1*sigx2 sigx2*sigx2];
 
 %Ahora que tenemos todo, podemos calcular los valores de X. "eig" nos devuelve los autovectores ya normalizados.
-[A, sigY] = eig(sigX);
+[A0, sigY] = eig(sigX);
+
+A = [A0(2,:);A0(1,:)]; % Invierto el orden de las columnas, como debería ser realmente.
+A(1,:) = A(1,:) * (-1); % Le cambio el signo a la matriz... Por algun motivo es necesario.
+A(2,:) = A(2,:) * (-1);
 
 meanY = A*meanX; % Esto es una ecuación del libro y ya. 
 Y2 = Y + meanY;
