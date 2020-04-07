2fda435c11ebb510ec8644aba81a9a8ed0ce3710
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2020-04-07 00:41:40 -0300

    Resulta que era sigma solo, supongo. Sigma cuadrado se va de tema.

--- TP1/Ejercicio-2/Matlab/main.m
+++ TP1/Ejercicio-2/Matlab/main.m
@@ -2,7 +2,7 @@ function [X, Y2, meanX, meanY, A, Ainv, sigX, sigY] = main(mean1, mean2, sigx1, 
 
 %1) Creamos el vector media de X y su matriz de covarianza sigma X.
 meanX = [mean1; mean2];
-sigX = [sigx1*sigx1, corr*sigx1*sigx2; corr*sigx1*sigx2, sigx2*sigx2];
+sigX = [sigx1, corr*sigx1*sigx2; corr*sigx1*sigx2, sigx2];
 
 %Ahora que tenemos eso, podemos calcular los valores de X. "eig" nos devuelve los autovectores ya normalizados.
 [A0, sigY0] = eig(sigX);
