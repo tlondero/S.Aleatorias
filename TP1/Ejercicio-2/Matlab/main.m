b0609c40ce01deebd06d57f6b754937c75065c60
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2020-04-08 11:11:44 -0300

    FAINARU APUDEITO-DESU

--- TP1/Ejercicio-2/Matlab/main.m
+++ TP1/Ejercicio-2/Matlab/main.m
@@ -1,4 +1,4 @@
-function [X, Y2, meanX, meanY, A, Ainv, sigX, sigY] = main(mean1, mean2, sigx1, sigx2, corr, samples)
+function [X, Y2, meanX, meanY, A, Ainv, sigX, sigY, xovar] = main(mean1, mean2, sigx1, sigx2, corr, samples)
 
 %1) Creamos el vector media de X y su matriz de covarianza sigma X.
 meanX = [mean1; mean2];
@@ -10,7 +10,7 @@ sigX = [sigx1*sigx1, corr*sigx1*sigx2; corr*sigx1*sigx2, sigx2*sigx2];
 A = [A0(2,:);A0(1,:)]; % Invierto el orden de las filas, que por algun motivo si no lo hago me invierte los ejes luego.
 sigY = [sigY0(2,2), 0;0, sigY0(1,1)];
 
-[y1, y2] = genX(sigY(1,1), sigY(2,2), samples); % Generamos Y, siendo sus varianzas los autovalores de sigma X.
+[y1, y2] = genX(sqrt(sigY(1,1)), sqrt(sigY(2,2)), samples); % Generamos Y, siendo sus varianzas los autovalores de sigma X.
 Y = [y1; y2]; %Prolijidad/Sintaxis.
 
 meanY = A*meanX; % Esto es una ecuación del libro y ya. 
@@ -24,4 +24,6 @@ axis equal
 figure(2)
 plot(X(1,:), X(2,:),'.');
 axis equal
+
+xovar = cov(X(1,:),X(2,:));
 end
