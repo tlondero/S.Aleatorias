16266567240cc3acc7d5966f538e979251e60773
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2020-06-06 21:32:55 -0300

    ajjaj dijo teta

--- TP2/Ejercicio-2/Matlab/mamodelgenerator.m
+++ TP2/Ejercicio-2/Matlab/mamodelgenerator.m
@@ -2,15 +2,21 @@ function [estx] = mamodelgenerator(x)
 
 sizeOfX = size(x);
 
-theta = thetaestimator(x);
-wgn = randn([1,sizeOfX(2)]);
+[thetas, min] = thetaestimator(x,1);
 
-estx = zeros(1, sizeOfX(2));
+theta21 = thetas(1); %Pongo cada theta en una variable con su subindice en el nombre
+theta22 = thetas(2);
+
+varN = min/(sizeOfX(2) - 2); %Calculo la varianza de e(n) por fórmula (pág 603)
+
+wgn = sqrt(varN)*randn([1,4096]); %Genero e(n). Media 0 y varianza ya calculada (La cual aplico multiplicando por el desvío estandar),
+
+estx = zeros(1, 4096); %Genero una muestra de X estimada a partir del modelo. Aca creo el vector.
 
 estx(1) = wgn(1)
-for n = 2:sizeOfX(2)
-    estx(n) = wgn(n) + wgn(n-1)*theta;
+estx(2) = wgn(2) - theta21*wgn(1);
+for n = 3:10000
+    estx(n) = wgn(n) + wgn(n-1)*theta21 + wgn(n-2)*theta22; %Aca lo completo, aplicando el modelo.
 end
-
-
+%Se devuelve el modelo.
 end
--- TP2/Ejercicio-2/Matlab/thetaestimator.m
+++ TP2/Ejercicio-2/Matlab/thetaestimator.m
@@ -1,34 +1,49 @@
-function [optimustheta] = thetaestimator(x)
-
+function [optimustheta, M] = thetaestimator(x, cantDec)
+   
+    %Con cantDec = 2 pasa a tardar +40 segundos, pero es mucho más preciso.
+    cantElem = 10^cantDec - 1; %Cantidad de elementos por cada lado
+    
     sizeOfArr = size(x);
     errorTable = zeros(1,sizeOfArr(2)); % Creo un arreglo del tamaño del vector X. Con esto, trabajaré a prueba y error.
     theta = 0;
-    totalErrorPerTheta = zeros(2,1+99*2) %El +1 es por el 0.
+    totalErrorPerTheta = zeros(1+cantElem*2,1+cantElem*2) %El +1 es por el 0.
     lengthOfTotalError = size(totalErrorPerTheta);
-    for m = -99:99
-        errorTable = zeros(1, sizeOfArr(2))
-        theta = m/100;
+    thetaindex = zeros(2,1+cantElem*2);
+    
+    for n = -(cantElem):(cantElem);
+        theta21 = n/(cantElem+1);
+        for m = -cantElem:cantElem
+            errorTable = zeros(1, sizeOfArr(2))
+            theta22 = m/(cantElem+1);
         
-        errorTable(1) = x(1);
+            errorTable(1) = x(1)
+            errorTable(2) = x(2) - theta21*errorTable(1)
+            for l = 3:sizeOfArr(2)
+               errorTable(l) = x(l) - theta21*errorTable(l-1) - theta22*errorTable(l-2);
+            end
         
-        for n = 2:sizeOfArr(2)
-            errorTable(n) = x(n) - theta*errorTable(n-1);
-        end
-        
-        for l = 1:sizeOfArr(2)
-        totalErrorPerTheta(1, m+100)= totalErrorPerTheta(1,m+100) + errorTable(l)*errorTable(l); %Sumo el cuadrado de cada componente del error.
-        %El 100 es para desplazar el m a un rango apropiado. Ej: Si m = -99 -> m + 100 = 1.
+            for j = 1:sizeOfArr(2)
+            totalErrorPerTheta(n+(cantElem+1), m+(cantElem+1)) = totalErrorPerTheta(n+(cantElem+1),m+(cantElem+1)) + errorTable(j)*errorTable(j); %Sumo el cuadrado de cada componente del error.
+            %El 100 es para desplazar el m a un rango apropiado. Ej: Si m = -99 -> m + 100 = 1.
             
+            end
         end
-        totalErrorPerTheta(2, m+100) = m/100;
     end
     
-    [M,P] = min(totalErrorPerTheta(1,1:lengthOfTotalError(2))); %Recupero el mínimo y su índice.
-    optimustheta = totalErrorPerTheta(2,P);
-
+    for n = -cantElem:cantElem
+        thetaindex(1, n+(cantElem+1)) = n/(cantElem+1);
+        thetaindex(2, n+cantElem+1) = n/(cantElem+1);
+    end
+    
+    [M,I] = min(totalErrorPerTheta(:)); %Recupero el mínimo y su índice.
+    I_col = floor(I/(cantElem*2+1)) + 1;
+    I_row = I - (I_col-1)*(cantElem*2+1);
+    optimustheta = [thetaindex(2, I_row), thetaindex(1,I_col)]; %Primero devuelve el theta21 y luego el 22.
 end
 
-% y = [0.5,0.99,-0.48,-0.2,-1.31,0.81,1.82,2.46,1.07,-1.29]
-%Esto es lo que uso para probar. Es el ejemplo 9.7 del libro. Me da bien, devuelve 0.48. Asumo que la data provista también me da bien
-%pero me resulta raro que me quede -0.99.
+%{ Usé el ejemplo del libro para est. Me da un poco distinto, pero revisé los valores como 8 veces y están bien.
+%y = [  1.102,     0.699,   2.336,     1.026,    -0.206,  1.486,     0.768,   0.69,     -0.001,    -1.150, -0.273,    -0.941,   1.746,     0.3,      -1.494,  0.192,     0.023,   1.633,     1.188,    -2.243, -0.961,    -0.77,    0.535,     1.263,     0.377, -0.48,      0.091,   1.007,     1.169,     0.346, -0.052,    -0.186,   0.033,     0.106,     1.402,  0.879,    -1.49,    1.031,     1.127,     0.824,-0.478,    -1.707,   0.877,     1.257,     0.83,-1.534,    -1.411,   0.468,    -0.066,    -1.56,-1.803,    -1.1,     0.274,     2.362,     1.834, 0.7780 ,   1.1720, -0.5030,   -0.100,    -1.527,-2.019,    -1.498,  -2.259,    -1.272,    -0.033,-0.096,    2.723 ,-0.243,    -2.256,    -0.462,  0.509,    -1.240,  -0.194,     0.217,     0.899, -0.683,    -0.621,   0.001,     0.978,     0.808, 0.419,     0.667,  -0.694,     0.355,     1.121, -0.130,    -0.895,  -1.366,    -0.247,     0.7410, 0.0470,   -0.662,  -1.2250,   -0.988,  -0.3130, 0.3120,   -0.7470, -1.8160,   -1.898,    -1.5920]
+%}
+
+
     
