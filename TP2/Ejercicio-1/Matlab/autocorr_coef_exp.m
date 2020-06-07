e41dd8d72444faf4efda513c91a10b109db11053
ftolaba1 <ftolaba@itba.edu.ar>
2020-06-07 14:44:53 -0300

    Me autocorregi unos comentarios

--- TP2/Ejercicio-1/Matlab/autocorr_coef_exp.m
+++ TP2/Ejercicio-1/Matlab/autocorr_coef_exp.m
@@ -1,10 +1,11 @@
 function [ coef_auto ] = autocorr_coef_exp( functions, fun_quant,t1, t2)
-%AUTOCORR_EXP autocorrelacion experimental entre dos instantes de tiempo
+%COEF_AUTO autocorrelacion experimental entre dos instantes de tiempo
 %t1 y t2 
 %functions , fun_quant = arreglo y tamano de funciones muestra
-%Para calcular la autocorrelacion se calcula el valor esperado del
-% producto entre los valores que adquieren las funciones muestra en los
-% instantes t1 y t2
+%Para calcular el coeficiente de autocorrelacion debemos calcular la
+%la autocorrelacion en (t1,t2), los valores esperados en t1 y t2
+%y por ultimo, la autocorrelacion en (t1,t1) y la autocorrelacion en
+%(t2,t2)
 
 autocorr = autocorr_exp(functions, fun_quant,t1, t2);
 exp_mean_t1 = expected_value(functions, fun_quant,t1);
