7ed87cb27d131c9e3baa72e1b9b00bcd6c1be9e3
ftolaba1 <ftolaba@itba.edu.ar>
2020-06-09 23:39:04 -0300

    Correccion de funciones estimadoras
    Ya se simul[o correctamente todos los valores, funciona todo bien!

--- TP2/Ejercicio-1/Matlab/autocorr_coef_exp.m
+++ TP2/Ejercicio-1/Matlab/autocorr_coef_exp.m
@@ -1,4 +1,4 @@
-function [ coef_auto ] = autocorr_coef_exp( functions, fun_quant,t1, t2)
+function [ coef_auto ] = autocorr_coef_exp(cant_muestras, muestras_t1, muestras_t2)
 %COEF_AUTO autocorrelacion experimental entre dos instantes de tiempo
 %t1 y t2 
 %functions , fun_quant = arreglo y tamano de funciones muestra
@@ -7,12 +7,12 @@ function [ coef_auto ] = autocorr_coef_exp( functions, fun_quant,t1, t2)
 %y por ultimo, la autocorrelacion en (t1,t1) y la autocorrelacion en
 %(t2,t2)
 
-autocorr = autocorr_exp(functions, fun_quant,t1, t2);
-exp_mean_t1 = expected_value(functions, fun_quant,t1);
-exp_mean_t2 = expected_value(functions, fun_quant,t2);
+autocorr = autocorr_exp(cant_muestras, muestras_t1, muestras_t2);
+exp_mean_t1 = expected_value(cant_muestras,muestras_t1);
+exp_mean_t2 = expected_value(cant_muestras, muestras_t2);
 
-autocorr_t1 = autocorr_exp(functions, fun_quant,t1, t1);
-autocorr_t2 = autocorr_exp(functions, fun_quant,t2, t2);
+autocorr_t1 = autocorr_exp(cant_muestras, muestras_t1, muestras_t1);
+autocorr_t2 = autocorr_exp(cant_muestras, muestras_t2, muestras_t2);
 
 coef_auto=(autocorr-(exp_mean_t1*exp_mean_t2))/sqrt(autocorr_t1*autocorr_t2);
 
--- TP2/Ejercicio-1/Matlab/var_exp.m
+++ TP2/Ejercicio-1/Matlab/var_exp.m
@@ -1,17 +1,13 @@
-function [ exp_var ] = var_exp( functions, fun_quant,t)
+function [ exp_var ] = var_exp(cant_muestras, muestras)
 %VAR_EXP Varianza experimental de arreglo de muestras
-% functions , fun_quant = arreglo y tamano de funciones muestra
-% t = valor de x en donde se quiere buscar la varianza del
-%           ensamble
+% cant_muestras, muestras = arreglo y tamano de funciones muestra
 %Para calcular la varianza experimental precisamos de la media
-%experimental y del valor cuadratico medio
-exp_meansqtr = 0;   %inicializo en 0
-exp_mean = expected_value(functions, fun_quant,t);
-%Calculo del valor cuadratico medio experimental
-for i=1:fun_quant 
-    exp_meansqtr = exp_meansqtr + (1/fun_quant)*(functions{i}(t))^2;
+%experimental 
+exp_var = 0;   %inicializo en 0
+exp_mean = expected_value(cant_muestras, muestras);
+%Estimamos la varianza experimental
+for i=1:cant_muestras 
+    exp_var = exp_var + (1/cant_muestras)*(muestras(i)- exp_mean)^2;
 end
-
-exp_var = exp_meansqtr - (exp_mean)^2;
 
 end
